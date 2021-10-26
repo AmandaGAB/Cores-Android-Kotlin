fb578c0401a22119f9fd3b58e6028796193c0333
AmandaGAB <amandagabsa@gmail.com>
2021-10-26 16:11:59 -0300

    Atualizado

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.myapplication
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var layout: ConstraintLayout
+    private lateinit var trgb: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,14 +19,16 @@ class MainActivity : AppCompatActivity() {
 
 
         this.layout = findViewById(R.id.background)
-
+        this.trgb = findViewById(R.id.Trgb)
 
 
 
         this.layout.setOnClickListener {
-
-            layout.setBackgroundColor(Color.rgb(Random.nextInt(1, 256),Random.nextInt(1, 256),Random.nextInt(1, 256)))
-
+            var redR = Random.nextInt(1, 256)
+            var blueR = Random.nextInt(1, 256)
+            var greenR = Random.nextInt(1, 256)
+            layout.setBackgroundColor(Color.rgb(redR,blueR,greenR))
+            trgb.text = "Red:"+(redR).toString()+"\nBlue:"+(blueR).toString()+"\nGreen:"+(greenR).toString()
         }
 
 
